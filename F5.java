81c778c69d9f9faf12f463401e85e2623a363581
Ana Zangirolani <103138017+anaoliz@users.noreply.github.com>
2022-04-06 15:11:09 -0300

    Create F5.java

---
+++ F5.java
@@ -0,0 +1,10 @@
+package listaF;
+
+import java.util.Scanner;
+
+public class F5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner meuScanner = new Scanner(System.in);
+		double ;
